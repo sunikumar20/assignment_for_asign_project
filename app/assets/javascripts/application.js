68704c87a9508564e6b2b344813b0891734bda29
sunil <sunil@ascratech.com>
2016-08-17 01:30:56 +0530

    make it better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,22 @@
 //= require bootstrap
 //= chart_loader
 //= require_tree .
+
+function startListHeight($tag) {
+  
+    function setHeight(s) {
+        var max = 0;
+        s.each(function() {
+            var h = $(this).height();
+            max = Math.max(max, h);
+        }).height('').height(max);
+    }
+  
+    $(window).on('ready load resize', setHeight($tag));
+}
+
+jQuery(function($) {
+    $('#list_lines').each(function() {
+        startListHeight($('.inr_list_lines', this));
+    });
+});
